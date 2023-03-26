50916d42d98512895509b306333a5d3d6712716c
NeverGetUppp <128945799+NeverGetUppp@users.noreply.github.com>
2023-03-26 16:38:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Anie lỏ",
+    introTitle: 'Anie',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao Anie cứ đi nhầm đường
+    Lạc vào tim NEET lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng Anie đã yêu NEET ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng Anie đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Trên thế giới hơn 8 tỉ người mà sao Anie lại yêu NEET <3',
+    btnReply: 'Gửi cho NEET <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    mess: 'NEET biết mà 🥰. Yêu Anie nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 9h, NEET qua nhà Anie chơi nha. 🥵',
+    btnAccept: 'Dạ ck iu :3 NEET muốn chơi gì cũng được :3 chơi đến sáng lun nha hihi :3💦💦',
+    messLink: 'https://www.facebook.com/messages/t/100009934027755' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
